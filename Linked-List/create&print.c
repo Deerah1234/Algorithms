fa3442df6fddb2c855156a5e2526dff22f2908e3
Emmanuel James <chiderajames55@gmail.com>
2023-06-01 17:29:55 +0100

    Create and Print LinkedList

---
+++ Linked-List/create&print.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+void print_nodes(struct node *head);
+
+int main() {
+	struct node *head, *newnode, *temp;
+	head = NULL;
+
+	int choice = 1;
+
+	while (choice)
+	{
+		newnode = malloc(sizeof(struct node));
+		newnode -> next = NULL;
+		printf("Enter value: ");
+		scanf("%d", &newnode->data);
+
+		if ( head == NULL ) {
+			head = temp = newnode;
+		} else {
+			temp->next = newnode;
+			temp = newnode;
+		}
+
+		printf("Do you want to continue? (0 or 1): ");
+		scanf("%d", &choice);
+	}
+
+	if (choice == 0) {
+		print_nodes(head);
+	}
+}
+
+void print_nodes(struct node *head) {
+    if (head == NULL)
+        printf("Linked List is empty");
+
+    struct node *ptr = head;
+
+    while (ptr != NULL) {
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
